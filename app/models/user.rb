9b4fba3a7e1ce7984ba0da20caf0707cc0dc1bf6
Frank Condezo <frankcondezo@gmail.com>
2023-09-08 13:30:26 -0500

    add annotate gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,23 @@ class User < ApplicationRecord
     [first_name, last_name].join(' ')
   end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  admin      :boolean          default(FALSE)
+#  email      :string
+#  first_name :string
+#  image      :string
+#  last_name  :string
+#  level      :integer
+#  username   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  unique_usernames  (username) UNIQUE
+#
