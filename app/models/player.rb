f0ed5966abb21bfb632305ab107079f854aa2f33
Frank Condezo <frankcondezo@gmail.com>
2023-09-08 18:24:17 -0500

    Feat initial pages with design (#2)

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,12 +1,2 @@
-# == Schema Information
-#
-# Table name: players
-#
-#  id         :bigint           not null, primary key
-#  level      :integer
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 class Player < ApplicationRecord
 end
