1099e797eea670d2ce0c97c69c0112ba2171f33a
Frank Condezo <frankcondezo@gmail.com>
2023-09-11 11:10:07 -0500

    add flash_message partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+    when 'success'
+      'bg-green-50 text-green-500'
+    when 'error'
+      'bg-red-50 text-red-500 '
+    when 'alert'
+      'bg-yellow-50 text-yellow-500'
+    when 'notice'
+      'bg-blue-500 text-white'
+    else
+      'bg-gray-500 text-white'
+    end
+  end
 end
