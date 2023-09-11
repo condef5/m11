73bd69a02653d2ec3920f5b0a8ae4499b8d0f620
Frank Condezo <frankcondezo@gmail.com>
2023-09-11 11:52:58 -0500

    Feat: add team random generation (#3)

---
+++ app/controllers/random_teams_controller.rb
@@ -0,0 +1,27 @@
+class RandomTeamsController < ApplicationController
+  def index
+    @random_team = RandomTeam.new(players_per_team: 7, list: session[:list])
+  end
+
+  def create
+    @random_team = RandomTeam.new(random_team_params)
+    session[:list] = @random_team.list
+
+    if @random_team.missing_players.any?
+      flash.now[:error] = 'Missing players in the list.'
+
+      render :index, status: :unprocessable_entity
+    else
+      @random_team.generate!
+      flash.now[:success] = 'Random teams were successfully created.'
+
+      render :index,  status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def random_team_params
+    params.require(:random_team).permit(:list, :players_per_team)
+  end
+end
