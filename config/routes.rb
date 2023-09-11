9bc59a7b17adb943a37849eee2bebbd380420b7b
Frank Condezo <frankcondezo@gmail.com>
2023-09-11 15:31:20 -0500

    add google controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   resources :players
   resources :random_teams, only: %i[index create]
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
+  get '/auth/google_oauth2', as: :sign_in
+  get '/sign_out', to: 'sessions#destroy', as: :sign_out
 end
