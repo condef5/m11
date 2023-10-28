ca448e3ca9b894f36a2adf6845781a47a096657a
Frank Condezo <frankcondezo@gmail.com>
2023-10-28 15:28:54 -0500

    update views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-  
+
   helper_method :current_user
 
   private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def success_notice(action, model)
+    t(action, model: model.class.model_name.human, scope: "activerecord.success")
   end
 end
