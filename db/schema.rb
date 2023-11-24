bc49f3a1644dc7034c9070ee4438b6abed6cdc8a
Frank Condezo <frankcondezo@gmail.com>
2023-11-24 02:22:21 -0500

    add support pg_trgm extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_08_065114) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_071917) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "answers", id: false, force: :cascade do |t|
