974cc4a85b5ae7494717c7567b90591639eb54d0
Frank Condezo <frankcondezo@gmail.com>
2023-11-30 01:24:16 -0500

    add custom validator form game_day

---
+++ app/models/game_day/list_validator.rb
@@ -0,0 +1,35 @@
+class GameDay::ListValidator
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :list, :string
+
+  def full_players
+    cleaned_list.map do |name|
+      founded_player = present_players.find { |player| player.name.downcase == name.downcase }
+      { name: name, player: founded_player }
+    end
+  end
+
+  def present_players
+    @players ||= Player.select(:name, :id)
+      .where('LOWER(name) IN (?)', player_names_from_list).distinct
+  end
+
+  def valid?
+    present_players.size == cleaned_list.count
+  end
+
+  private
+
+  def player_names_from_list
+    cleaned_list.map(&:downcase)
+  end
+
+  def cleaned_list
+    list.split("\n")
+    .map(&:strip)
+    .reject(&:empty?)
+    .map { |line| line.gsub(/^\d+\.\s*/, '') }
+  end
+end
