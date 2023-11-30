8133ee21080a539daaa3a37cfcd477c83fd9f474
Frank Condezo <frankcondezo@gmail.com>
2023-11-30 01:50:59 -0500

    add selects for missing players in random team

--- app/controllers/random_teams_controller.rb
+++ app/controllers/random_teams_controller.rb
@@ -1,20 +1,22 @@
 class RandomTeamsController < ApplicationController
   def index
-    @random_team = RandomTeam.new(players_per_team: 7, list: session[:list] || "")
+    @random_team = RandomTeam.new(players_per_team: session[:players_per_team], list: session[:list] || "")
+    @validator =  GameDay::ListValidator.new(list: session[:list] || "")
   end
 
   def create
     @random_team = RandomTeam.new(random_team_params)
+    @validator = GameDay::ListValidator.new(list: @random_team.list)
     session[:list] = @random_team.list
+    session[:players_per_team] = @random_team.players_per_team
 
-    if @random_team.missing_players.any?
-      flash.now[:error] = 'Missing players in the list.'
-
-      render :index, status: :unprocessable_entity
-    else
+    if @validator.valid?
       @random_team.generate!
       flash.now[:success] = 'Random teams were successfully created.'
 
+      render :index, status: :unprocessable_entity
+    else
+      flash.now[:alert] = 'Missing players in the list.'
       render :index,  status: :unprocessable_entity
     end
   end
